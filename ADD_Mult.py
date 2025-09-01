14c4ffc9cbfb5f28956ff3e688028f58ae9ae390
rahuldas1222bwu-rgb <rahuldas1222bwu@gmail.com>
2025-09-01 15:48:43 +0530

    Create ADD_Mult.py

---
+++ ADD_Mult.py
@@ -0,0 +1,10 @@
+import numpy as np
+a =np.array[[1,2,3],
+            [4,5,6],
+            [7,8,9]]
+b = np.array[[10,21,32],
+             [44,54,61],
+             [71,18,19]]
+print (a+b)
+c = np.dot(a,b)
+print(c)
